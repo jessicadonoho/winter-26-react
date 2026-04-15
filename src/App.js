e1e167c32bc9d77d4fbb9f72e1dafafaf957ef87
Jessica Donoho <jesiedu@jesiluv-3.local>
2026-04-15 10:13:52 -0400

    bruh

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Random Change
         </a>
       </header>
     </div>
